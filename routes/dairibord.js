561df9477cbcfc84d0f07704e9d4c0519902958b
Tapiwa Jeka <tapiwajeka@gmail.com>
2020-05-31 18:32:48 +0200

    minor update

--- routes/dairibord.js
+++ routes/dairibord.js
@@ -118,70 +118,97 @@ router.post('/', function (req, res) {
                         return; 
                     }
                     var stage = await getLastStage(message);
-                    if (stage == 'ASK'){
-                       var txt = 'Hi! *' + message.text + '*\n';
-                        saveContactName(message,message.text);
+                    console.log("STAGE: ",stage);
+                    if (stage.stage == 'ASK'){
+                       var txt = 'Hi! *' + message.body + '*\n';
+                        saveContactName(message,message.body);
                         txt += 'I am very pleased to meet you.\n\n';
                         
-                        var txt1 = await getMenuText('A',message);
-                        txt += txt1;
-                        return txt;
+                        var m = await getMenuText('A',message);
+                        m.msg = txt + m.msg;
+                        //sendMessage(message, m);
+                        return m.msg;
                     }
-                    if (isGreeting(message.text) == true) {
-                        var txt = await getMenuText('A',message);
+                    if ((isGreeting(message.body) == true)) {
+                        var m = await getMenuText('A',message);
+                        //sendMessage(message, m);
                     } else if (isBackButton(message) == true){
                         console.log('BACK');
                         var newStage = await getPreviousStage(message);
                         console.log('Prev Stage:', newStage);
                         if (typeof(newStage === 'object')){
-                            var txt = await getStageText(newStage, message);
-                            //var txt = await getMenuText(newStage.stage,message);
+                            var m = await getStageText(newStage, message);
+                            //sendMessage(message,m);
                         } else {
-                            var txt = await getMenuText('A',message);
+                            var m = await getMenuText('A',message);
+                            //sendMessage(message, m);
                         }
-                        checkMyResponse(txt);
+                        checkMyResponse(m);
                     } else if (isLoggingComplaint(stage) == true){
                         console.log("Logging complaint");
                          processComplaint(message,stage);
                     }
                     else {
                         var opt = await isMenuOption(stage,message);
-                        //console.log('opt',opt);
+                        console.log('opt',opt);
                         if (opt>0) { 
                             // implies that the option is available on the list of presented menu options
                             var newStage = await getNextMenu(stage,opt);
-                            //console.log('Next Stage:' +newStage);
-                            var txt = await getMenuText(newStage,message);
-                            checkMyResponse(txt);
+                            console.log('Next Stage:' +newStage);
+                            var m = await getMenuText(newStage,message);
+                            
+                            //sendMessage(message,m);
+                            checkMyResponse(m);
                         } else if (opt == 0) { // implies that the option is NOT available on the list of presented menu options
                             //check if it's a content option
                             var msg = '';
-                            if ((stage == 'content')||(stage == 'menu')){
-                               msg = await processContentOption(message);
-                               checkMyResponse(msg);
+                            if ((stage.stage == 'content')||(stage.stage == 'menu')){
+                            txt = await processContentOption(message);
+                            var m = {"msg": txt,
+                                        "stage": stage.stage,
+                                        "stage_type": stage.stage_type,
+                                        "stageDetails": []};
+                            //sendMessage(message, m);
+                            checkMyResponse(msg);
                             }
                             if (msg =='') {
                                 //else, present first menu
                                 var msg = await getMenuText('A',message);
+                                //sendMessage(message, msg);
                             }
+
                         } else if (opt == -1) {
                             //implies that the response is not a number therefore
                             // 1. search for the response in the list of menu options
+                            console.log("1. search for the response in the list of menu options");
                             var txt = await getMenuFromText(message);
-                            checkMyResponse(txt);
-                            if (txt === '') {
+                            var m = {"msg": txt,
+                                        "stage": stage.stage,
+                                        "stage_type": stage.stage_type,
+                                        "stageDetails": []};
+                            console.log(m);
+                            //sendMessage(message,m);
+                            checkMyResponse(m);
+                            if (m.msg === '') {
                             // 2. search for the response in the content
-                                var msg = await getContentFromText(message);
-                                if ( msg != '') {
-                                    checkMyResponse(msg);
+                                console.log(" 2. search for the response in the content");
+                                var m = await getContentFromText(stage,message);
+                                if ( m.msg != '') {
+                                    return m;
+                                    //sendMessage(message, m);
+                                    checkMyResponse(m);
                                 } else {
                                     //invalid response therefore, present first menu
-                                    var msg = await getMenuText('A',message);
+                                    console.log("invalid response therefore, present first menu");
+                                    txt = await getMenuText('A',message);
+                                    return msg;
+                                    //sendMessage(message, msg);
                                 }
                             }
                         }
+                
                     }   
-                    return txt;
+                    return m.msg;
                 }
 
                 
